12e19a019ef23386304c826f49b7031ff7a4a5b4
Regina Musovirova <musovirova@yahoo.com>
2020-02-21 12:25:44 -0500

    adding files

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Code-Quiz
+
+# Plan
+
+1. When we click on the Start's button, menu swich to the question's quiz. At the same time the timer should start working.
+2. When we answer questions an alert apears with Right or Wrong text.
+3. When we answer all the questions, a window change to the total score page where you could save it to the storage and clear the scores if need.
+4. Quiz will end after the time has elapsed even if we do not answer all the questions.
---
+++ questions.js
@@ -0,0 +1,34 @@
+// list of all questions, choices, and answers
+var questions = [
+  {
+    title: "How to write an IF statement for executing some code if i is NOT equal to 5?",
+    choices: ["if i <> 5", "if (i !=5)", "if i =! 5 then", "if (i<>5)"],
+    answer: "if (i !=5)"
+  },
+  {
+    title: "The condition in an if / else statement is enclosed within ____.",
+    choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+    answer: "parentheses"
+  },
+  {
+    title: "How do you create a function in JavaScript?",
+    choices: [
+      "function = myFunction()",
+      "function:myFunction()",
+      "function myFunction()"
+    ],
+    answer: "function myFunction()"
+  },
+  {
+    title:
+      "String values must be enclosed within ____ when being assigned to variables.",
+    choices: ["commas", "curly brackets", "quotes", "parentheses"],
+    answer: "quotes"
+  },
+  {
+    title:
+    "Where is the correct place to insert a JavaScript?",
+    choices: ["The <head> section", "The <body> section", "Both"],
+    answer: "Both"
+  }
+];
